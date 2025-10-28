06c5859aff8cdc8a8ed288d7e68a80e1861f6551
Bogachev Dmitrii <dm.bogachev@yandex.ru>
2025-10-28 11:39:12 +0300

    Small fixi

--- cv-service/app/FrameProcessor.py
+++ cv-service/app/FrameProcessor.py
@@ -2,6 +2,7 @@
 import os, sys, json, threading, time, math
 
 from sympy.geometry.entity import scale
+
 # Добавляем директорию проекта в sys.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -31,13 +32,14 @@ REDIS_CAMERA_FRAME_KEY = os.getenv("REDIS_CAMERA_FRAME_KEY", "camera_frame")
 REDIS_PROCESSED_FRAME_KEY = os.getenv("REDIS_PROCESSED_FRAME_KEY", "processed_frame")
 
 if is_docker():
-   DATA_DIR = "/data"
+    DATA_DIR = "/data"
 else:
-   DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
+    DATA_DIR = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data"
+    )
 
 
 class FrameProcessor:
-
     def __init__(self):
         logger.debug("Инициализация обработчика кадров")
         self.__cuda_available = torch.cuda.is_available()
@@ -61,11 +63,12 @@ class FrameProcessor:
 
         self.__process_thread = threading.Thread(target=self.__process_loop).start()
 
-        
     def __load_coefficients(self):
         """Загружает матрицу камеры и коэффициенты настроечного оптического дефекта."""
         try:
-            data = np.load(os.path.join(DATA_DIR, Config.get("CameraDistortionCoeffsName")))
+            data = np.load(
+                os.path.join(DATA_DIR, Config.get("CameraDistortionCoeffsName"))
+            )
             self.__cameraMatrix = data["cameraMatrix"]
             self.__distCoeffs = data["distCoeffs"]
             logger.debug(f"Матрица камеры:\n{self.__cameraMatrix}")
@@ -75,75 +78,40 @@ class FrameProcessor:
             self.__cameraMatrix = None
             self.__distCoeffs = None
 
-    # def __undistort(self, frame):
-    #     distorted = frame.copy()
-    #     if self.__cameraMatrix is not None and self.__distCoeffs is not None:
-            
-    #         if not self.__cuda_available:
-    #             Use remap for CUDA-based undistortion
-    #             h, w = distorted.shape[:2]
-    #             map1, map2 = cv2.initUndistortRectifyMap(
-    #                 self.__cameraMatrix, self.__distCoeffs, None, 
-    #                 self.__cameraMatrix, (w, h), cv2.CV_32FC1
-    #             )
-    #             gpu_frame = cv2.cuda_GpuMat()
-    #             gpu_map1 = cv2.cuda_GpuMat()
-    #             gpu_map2 = cv2.cuda_GpuMat()
-                
-    #             gpu_frame.upload(distorted)
-    #             gpu_map1.upload(map1)
-    #             gpu_map2.upload(map2)
-                
-    #             gpu_undistorted = cv2.cuda.remap(
-    #                 gpu_frame, gpu_map1, gpu_map2, 
-    #                 interpolation=cv2.INTER_LINEAR
-    #             )
-    #             undistorted = gpu_undistorted.download()
-    #             logger.debug("Устранение дисторсии выполнено")
-    #             return undistorted
-    #         else:
-    #             undistorted = cv2.undistort(distorted, self.__cameraMatrix, self.__distCoeffs)
-    #             logger.debug("Устранение дисторсии выполнено")
-    #             return undistorted
-    #     logger.debug("Устранение дисторсии не выполнено")
-    #     return distorted
-
     def __get_frame_from_redis(self):
-        ''' Получает кадр из Redis '''
+        """Получает кадр из Redis"""
         frame_data = redis_client.get(REDIS_CAMERA_FRAME_KEY)
         if frame_data:
             np_arr = np.frombuffer(frame_data, np.uint8)
             img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
             return img
         return None
-    
-    def __put_frame_to_redis(self, frame):
-        ''' Помещает обработанный кадр обратно в Redis '''
 
-        _, buffer = cv2.imencode('.jpg', frame)
+    def __put_frame_to_redis(self, frame):
+        """Помещает обработанный кадр обратно в Redis"""
+
+        _, buffer = cv2.imencode(".jpg", frame)
         redis_client.set(REDIS_PROCESSED_FRAME_KEY, buffer.tobytes())
 
     def __process_uncalibrated(self, frame):
-        ''' Обрабатывает некалиброванный кадр '''
+        """Обрабатывает некалиброванный кадр"""
 
         markers = self.__find_markers(frame)
         if markers and len(markers) > 0:
             logger.debug(f"Обнаружено {len(markers)} маркерa(ов) ArUco")
             self.__draw_markers(frame, markers)
         self.__objects = None
-        # self.__find_circles(frame)
         self.__put_frame_to_redis(frame)
 
     def __process_calibrated(self, frame):
-        ''' Обрабатывает откалиброванный кадр '''
+        """Обрабатывает откалиброванный кадр"""
         try:
             frame = self.__prepare_frame(frame)
-            
+
             predictions = self.detector.detect(frame)
             if predictions and len(predictions) > 0:
                 frame, predictions = self.function.process(frame, predictions)
             if predictions and len(predictions) > 0:
-                # logger.debug(f"0 элемент до масштабирования: {predictions[0].pick_point}")
                 frame = self.drawer.draw(frame, predictions)
                 self.__objects = predictions
 
@@ -157,18 +125,12 @@ class FrameProcessor:
                 for y in range(0, frame.shape[0], 100):
                     cv2.line(frame, (0, y), (frame.shape[1], y), (0, 255, 0), 1)
 
-            # frame = cv2.drawMarker(frame, (1000, 1000), (0,0,255), cv2.MARKER_CROSS, 5, 8)
-            # frame = cv2.drawMarker(frame, (2500, 1500), (0,0,255), cv2.MARKER_CROSS, 5, 8)
-            # frame = cv2.drawMarker(frame, (3000, 2500), (0,0,255), cv2.MARKER_CROSS, 5, 8)
-            # frame = cv2.drawMarker(frame, (4500, 1500), (0,0,255), cv2.MARKER_CROSS, 5, 8)
-
-
             self.__put_frame_to_redis(frame)
         except Exception as e:
             logger.error(f"Ошибка при обработке кадра: {e}")
 
     def __prepare_frame(self, frame):
-        ''' Подготавливает кадр для обработки с учётом перспективной калибровки и CUDA '''
+        """Подготавливает кадр для обработки с учётом перспективной калибровки и CUDA"""
 
         if not self.calibrator.Calibrated:
             logger.warning("Калибровка не выполнена — кадр не обработан")
@@ -186,7 +148,9 @@ class FrameProcessor:
                     gpu_frame = cv2.cuda_GpuMat()
                     gpu_frame.upload(frame)
 
-                    gpu_warped = cv2.cuda.warpPerspective(gpu_frame, self.calibrator.M, target_size)
+                    gpu_warped = cv2.cuda.warpPerspective(
+                        gpu_frame, self.calibrator.M, target_size
+                    )
                     result = gpu_warped.download()
                 except Exception as e:
                     logger.error(f"Ошибка CUDA-преобразования: {e}")
@@ -201,9 +165,8 @@ class FrameProcessor:
             logger.error(f"Ошибка при подготовке кадра: {e}")
             return frame
 
-
     def __process_loop(self):
-        ''' Запускает цикл обработки кадров '''
+        """Запускает цикл обработки кадров"""
 
         logger.debug("Запуск цикла обработки кадров")
         self.process_started = True
@@ -216,7 +179,7 @@ class FrameProcessor:
                 time.sleep(5)
                 continue
             logger.debug("Кадр успешно получен из Redis, начинаем обработку")
-            
+
             if not self.calibrator.Calibrated:
                 self.__process_uncalibrated(frame)
             else:
@@ -225,20 +188,44 @@ class FrameProcessor:
             time.sleep(Config.get("Process.ProcessingDelay", 1))
 
     def __find_markers(self, frame):
-        ''' Находит маркеры ArUco на кадре '''
+        """Находит маркеры ArUco на кадре"""
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         markers = self.__aruco.detectMarkers(gray)
         return markers
 
     def __draw_markers(self, frame, markers):
-        ''' Рисует обнаруженные маркеры на кадре '''
+        """Рисует обнаруженные маркеры на кадре"""
         for id, marker in markers.items():
-            cv2.drawMarker(frame, marker.center, (255,0,0), cv2.MARKER_CROSS, 5, 8)
-            cv2.putText(frame, f"ID: {id}", (marker.center[0] + 10, marker.center[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
-            cv2.polylines(frame, [np.array([marker.topLeft, marker.topRight, marker.bottomRight, marker.bottomLeft], dtype=np.int32)], isClosed=True, color=(0, 255, 0), thickness=2)
+            cv2.drawMarker(frame, marker.center, (255, 0, 0), cv2.MARKER_CROSS, 5, 8)
+            cv2.putText(
+                frame,
+                f"ID: {id}",
+                (marker.center[0] + 10, marker.center[1]),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 0, 0),
+                2,
+            )
+            cv2.polylines(
+                frame,
+                [
+                    np.array(
+                        [
+                            marker.topLeft,
+                            marker.topRight,
+                            marker.bottomRight,
+                            marker.bottomLeft,
+                        ],
+                        dtype=np.int32,
+                    )
+                ],
+                isClosed=True,
+                color=(0, 255, 0),
+                thickness=2,
+            )
 
     def calibrate(self):
-        ''' Выполняет калибровку на текущем кадре '''
+        """Выполняет калибровку на текущем кадре"""
         frame = self.__get_frame_from_redis()
         if frame is None:
             logger.warning("Не удалось получить кадр из Redis для калибровки")
@@ -252,28 +239,41 @@ class FrameProcessor:
         return success
 
     def set_processing_delay(self, delay):
-        ''' Устанавливает задержку между обработкой кадров '''
+        """Устанавливает задержку между обработкой кадров"""
         Config.set("Process.ProcessingDelay", delay)
         Config.save()
         logger.info(f"Задержка между обработкой кадров установлена на {delay} с")
 
-    def add_model(self, model_name, model_file_name, confidence_threshold, model_type, processor_file):
-        ''' Добавляет новую модель в конфигурацию '''
+    def add_model(
+        self,
+        model_name,
+        model_file_name,
+        confidence_threshold,
+        model_type,
+        processor_file,
+    ):
+        """Добавляет новую модель в конфигурацию"""
         model_info = {
             "ModelName": model_name,
             "ModelFileName": model_file_name,
             "ConfidenceThreshold": confidence_threshold,
             "ModelType": model_type,
-            "ModelProcessor": processor_file
+            "ModelProcessor": processor_file,
         }
         Config.set(f"Models.{model_name}", model_info)
         Config.save()
-        self.detector.add_model(model_name, model_file_name, confidence_threshold, model_type, processor_file)
+        self.detector.add_model(
+            model_name,
+            model_file_name,
+            confidence_threshold,
+            model_type,
+            processor_file,
+        )
 
         logger.info(f"Модель {model_info['ModelName']} добавлена в конфигурацию")
 
     def change_model(self, model_name):
-        ''' Смена модели детектора '''
+        """Смена модели детектора"""
         if self.detector.change_model(model_name):
             logger.info(f"Модель изменена на {model_name}")
             self.function.load_function(self.detector.get_current_yolo_model())
@@ -283,37 +283,24 @@ class FrameProcessor:
             return False
 
     def get_objects(self):
-        ''' Получение объектов '''
+        """Получение объектов"""
         return self.__objects
 
     def get_first_object(self):
-        ''' Получение первого объекта '''
+        """Получение первого объекта"""
         if self.__objects:
             return self.__objects[0]
         return None
 
-    def transform_point(self, point, M):
-        # Преобразуем точку в однородные координаты
-        point_homogeneous = np.array([point[0], point[1], 1])
-        
-        # Умножаем на матрицу преобразования
-        transformed = np.dot(M, point_homogeneous)
-        
-        # Нормализуем координаты
-        x = int(transformed[0] / transformed[2])
-        y = int(transformed[1] / transformed[2])
-        
-        return (x, y)
 
 if __name__ == "__main__":
     processor = FrameProcessor()
     cv2.namedWindow("Processed Frame", cv2.WINDOW_FREERATIO)
     while True:
-
         key = cv2.waitKey(1)
-        if key == ord('q'):
+        if key == ord("q"):
             break
-        if key == ord('c'):
+        if key == ord("c"):
             processor.calibrate()
-        if key == ord('r'):
+        if key == ord("r"):
             processor.calibrator.uncalibrate()
