75d97da5e4e0f690938f7b73be36845f037d826e
Bogachev Dmitrii <dm.bogachev@yandex.ru>
2025-12-10 13:00:45 +0300

    Added without me

--- data/postprocessors/proc_0401.17.02.023.py
+++ data/postprocessors/proc_0401.17.02.023.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Внутренние модули
 from common.Logger import config_logger
-logger = config_logger("proc_440.00.26.py")
+logger = config_logger("proc_0401.17.02.023.py")
 
 def process(frame, yolo_data):
     process_frame = frame.copy()
@@ -19,12 +19,12 @@ def process(frame, yolo_data):
         center = (x1 + x2) / 2, (y1 + y2) / 2
         data.pick_point = center
         logger.debug(f"Ключевые точки: {data.keypoints}")
-        if data.keypoints[0][1] > data.keypoints[1][1]:
-            data.pick_angle = 0
-            data.pick_point = (data.pick_point[0], data.pick_point[1] - 0.8*abs(data.keypoints[0][1] - data.keypoints[1][1]))
-        else:
+        if data.keypoints[0][1] < data.keypoints[1][1]:
             data.pick_angle = 180
-            data.pick_point = (data.pick_point[0], data.pick_point[1] + 0.8*abs(data.keypoints[0][1] - data.keypoints[1][1]))
+            data.pick_point = (data.pick_point[0], data.pick_point[1] - (-0.6)*abs(data.keypoints[0][1] - data.keypoints[1][1]))
+        else:
+            data.pick_angle = 0
+            data.pick_point = (data.pick_point[0], data.pick_point[1] + (-0.8)*abs(data.keypoints[0][1] - data.keypoints[1][1]))
         logger.debug(f"Угол забора: {data.pick_angle}")
         result.append(data)
                 
